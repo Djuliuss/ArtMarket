887a7944015d14bcd08672596d78dbf8d02de948
Julio Delgado <juliods@gmail.com>
2018-10-03 10:41:52 +0100

    question 3.1 works

--- question3-nodejs/finalexam/copyCerts.sh
+++ question3-nodejs/finalexam/copyCerts.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -f app/certs/*sk 
+rm -f app/certs/*pem
 cp /home/julio/hlf/FinalExam/question3-nodejs/finalexam/org3-artifacts/crypto-config/peerOrganizations/Org3.com/users/Admin@Org3.com/msp/admincerts/Admin@Org3.com-cert.pem ./app/certs -r
 cp /home/julio/hlf/FinalExam/question3-nodejs/finalexam/org3-artifacts/crypto-config/peerOrganizations/Org3.com/users/Admin@Org3.com/msp/keystore/* ./app/certs -r
 
@@ -7,7 +9,7 @@ cd app/certs
 awk '{printf "%s\\n", $0}' Admin\@Org3.com-cert.pem > Org3AdminFormatted.pem
 
 
-cp b1fe17738f8d1d054d3f57f14bfaf2c61d1bd775db4c7e1d6ed41ad500fea844_sk ~/.hfc-key-store/b1fe17738f8d1d054d3f57f14bfaf2c61d1bd775db4c7e1d6ed41ad500fea844-priv
+cp 995ad705b8ea0d249607403f7d682ff56adcca837a90bc75de87c2fa39bfedfb_sk ~/.hfc-key-store/995ad705b8ea0d249607403f7d682ff56adcca837a90bc75de87c2fa39bfedfb-priv
 
 
 
