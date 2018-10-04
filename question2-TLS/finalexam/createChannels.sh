d39f780a2d0e6c71033c50334c29d97c0e4437a2
Julio Delgado <juliods@gmail.com>
2018-10-04 16:39:57 +0100

    tls finished

---
+++ question2-TLS/finalexam/createChannels.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker exec cli.Org1 bash -c 'peer channel create -c test -f ./channels/Test.tx -o orderer.finalexam.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/finalexam.com/orderers/orderer.finalexam.com/msp/tlscacerts/tlsca.finalexam.com-cert.pem'
+
+docker exec cli.Org1 bash -c 'peer channel join -b test.block'
+docker exec cli.Org2 bash -c 'peer channel join -b test.block'
+
+docker exec cli.Org1 bash -c 'peer channel update -o orderer.finalexam.com:7050 -c test -f ./channels/Org1anchor.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/finalexam.com/orderers/orderer.finalexam.com/msp/tlscacerts/tlsca.finalexam.com-cert.pem'
+docker exec cli.Org2 bash -c 'peer channel update -o orderer.finalexam.com:7050 -c test -f ./channels/Org2anchor.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/finalexam.com/orderers/orderer.finalexam.com/msp/tlscacerts/tlsca.finalexam.com-cert.pem'
---
+++ question2-TLS/finalexam/runChaincode.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker exec cli.Org1 bash -c "peer chaincode invoke -C test -n finalexam -c '{\"Args\":[\"createPieceOfArt\", \"Org1001\", \"Mona Lisa\", \"Painting\"]}' --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/finalexam.com/orderers/orderer.finalexam.com/msp/tlscacerts/tlsca.finalexam.com-cert.pem"
+
+docker exec cli.Org1 bash -c "peer chaincode invoke -C test -n finalexam -c '{\"Args\":[\"createPieceOfArt\", \"Org1002\", \"The Scream\", \"Painting\"]}' --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/finalexam.com/orderers/orderer.finalexam.com/msp/tlscacerts/tlsca.finalexam.com-cert.pem"
+
+docker exec cli.Org2 bash -c "peer chaincode invoke -C test -n finalexam -c '{\"Args\":[\"createPieceOfArt\", \"Org2001\", \"Happiness\", \"Picture\"]}' --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/finalexam.com/orderers/orderer.finalexam.com/msp/tlscacerts/tlsca.finalexam.com-cert.pem"
+
+docker exec cli.Org2 bash -c "peer chaincode invoke -C test -n finalexam -c '{\"Args\":[\"createPieceOfArt\", \"Org2003\", \"Venus\", \"Vase\"]}' --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/finalexam.com/orderers/orderer.finalexam.com/msp/tlscacerts/tlsca.finalexam.com-cert.pem"
+
+sleep 5
+
+docker exec cli.Org1 bash -c "peer chaincode query -C test -n finalexam -c '{\"Args\":[\"queryPieceOfArt\", \"Org1001\"]}'"
+
+docker exec cli.Org2 bash -c "peer chaincode query -C test -n finalexam -c '{\"Args\":[\"queryPieceOfArt\", \"Org1002\"]}'"
+
