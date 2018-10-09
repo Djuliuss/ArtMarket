d61924ddfc93d92039b063cfd262767f51462a20
Julio Delgado <juliods@gmail.com>
2018-10-09 22:05:05 +0100

    adding comments

--- question2/finalexam/initArtifacts.sh
+++ question2/finalexam/initArtifacts.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# the following commands generate the crypto material for the orderer, Org1 and Org2.
 sudo rm -rf crypto-config
 
 cryptogen generate --config crypto-config.yaml
 
-
+# the following commands generate the genesis block for the orderer.
 sudo rm -rf orderer ; mkdir orderer
 
 configtxgen -profile finalexamOrdererGenesis -outputBlock ./orderer/genesis.block
 
 
+# the following commands generate the channel artifacts necessary to create the channel and the anchor peer update 
+# transactions
 sudo rm -rf channels ; mkdir channels
 
 configtxgen -profile TestChannel -outputCreateChannelTx ./channels/Test.tx -channelID test
--- question2/finalexam/runChaincode.sh
+++ question2/finalexam/runChaincode.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# we create a few Pieces of Art in both Org1 and Org2
+
 docker exec cli.Org1 bash -c "peer chaincode invoke -C test -n finalexam -c '{\"Args\":[\"createPieceOfArt\", \"Org1001\", \"Mona Lisa\", \"Painting\"]}'"
 
 docker exec cli.Org1 bash -c "peer chaincode invoke -C test -n finalexam -c '{\"Args\":[\"createPieceOfArt\", \"Org1002\", \"The Scream\", \"Painting\"]}'"
@@ -8,8 +10,11 @@ docker exec cli.Org2 bash -c "peer chaincode invoke -C test -n finalexam -c '{\"
 
 docker exec cli.Org2 bash -c "peer chaincode invoke -C test -n finalexam -c '{\"Args\":[\"createPieceOfArt\", \"Org2003\", \"Venus\", \"Vase\"]}'"
 
+# we need to wait a few secods for the changes to be commited into the blockchain
+
 sleep 5
 
+# we query the blockchain to validate the records have been inserted into the blockchain correctly.
 docker exec cli.Org1 bash -c "peer chaincode query -C test -n finalexam -c '{\"Args\":[\"queryPieceOfArt\", \"Org1001\"]}'"
 
 docker exec cli.Org2 bash -c "peer chaincode query -C test -n finalexam -c '{\"Args\":[\"queryPieceOfArt\", \"Org1002\"]}'"
